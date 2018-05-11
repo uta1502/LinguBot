0ed377901ebac2189d74b9fd99d29b26af009b1c
Sajid Ibne Anower <sajidanower23@gmail.com>
2018-05-12 03:15:21 +1000

    Add initial parsing and types

---
+++ my_parse.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+"""
+Custom types
+"""
+
+from enum import Enum
+
+class Mode(Enum):
+    live = 'live'
+    audio_file = 'audio_file'
+
+    def __str__(self):
+        return self.value
+
+class Platform(Enum):
+    fb = 'fb'
+    slack = 'slack'
+
+    def __str__(self):
+        return self.value
+
+# parser = argparse.ArgumentParser()
+# parser.add_argument("echo", help="echo the string you use here")
+# parser.add_argument("--prod", help="Run this in production", action="store_true")
+# parser.add_argument("-m", "--mode", help="How to read in your speech", type=Mode, choices=list(Mode))
+# # parser.add_argument("--platform", help="Which platform to run this on", type=Platform, choices=list(Platform))
+# args = parser.parse_args()
+# print(args.mode)
