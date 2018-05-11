2a95934fac6773770f2402a63570f7986de6c5e8
Sajid Ibne Anower <sajidanower23@gmail.com>
2018-05-12 04:14:51 +1000

    Complete integration of cmd args with audio_to_speech

--- arg_parser.py
+++ arg_parser.py
@@ -1,12 +1,21 @@
 #!/usr/bin/env python
 import argparse
 from my_types import Mode, Platform
+from lang import languages, countries
 
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("echo", help="echo the string you use here")
     parser.add_argument("--prod", help="Run this in production", action="store_true")
-    parser.add_argument("-m", "--mode", help="How to read in your speech", type=Mode, choices=list(Mode))
-    parser.add_argument("--platform", help="Which platform to run this on", type=Platform, choices=list(Platform))
+    parser.add_argument("-m", "--mode", help="How to read in your speech",
+                        type=Mode, default=Mode.live, choices=list(Mode))
+    parser.add_argument("--platform", help="Which platform to run this on",
+                        type=Platform, default=Platform.fb, choices=list(Platform))
+    parser.add_argument("-l", "--lang", help="Which language to translate to",
+                        type=str, default="en-US", choices=list(languages))
+    parser.add_argument("files", nargs="+", help="Which files to read. Only when --mode=audio_files")
+
     args = parser.parse_args()
     return args
+
+if __name__ == '__main__':
+    print(get_args())
--- my_types.py
+++ my_types.py
@@ -19,11 +19,3 @@ class Platform(Enum):
 
     def __str__(self):
         return self.value
-
-# parser = argparse.ArgumentParser()
-# parser.add_argument("echo", help="echo the string you use here")
-# parser.add_argument("--prod", help="Run this in production", action="store_true")
-# parser.add_argument("-m", "--mode", help="How to read in your speech", type=Mode, choices=list(Mode))
-# # parser.add_argument("--platform", help="Which platform to run this on", type=Platform, choices=list(Platform))
-# args = parser.parse_args()
-# print(args.mode)
