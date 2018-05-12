ff0075d26995027e152cad28356220cd2485dbb9
Sajid Ibne Anower <sajidanower23@gmail.com>
2018-05-12 17:08:48 +1000

    Tweaks

--- options.py
+++ options.py
@@ -4,6 +4,13 @@ import sqlite3
 from db import conn
 from audio_to_text import DEFAULT_DEST, DEFAULT_SRC
 
+def set_defaults(options_dict):
+    if 'dest' not in options_dict:
+        options_dict['dest'] = DEFAULT_DEST
+    if 'src' not in options_dict:
+        options_dict['src'] = DEFAULT_SRC    
+    return options_dict
+
 # options_str looks like "#options dest:en src:fr"
 def parse_options(options_str):
     tokens = options_str.split()[1:]
@@ -11,20 +18,27 @@ def parse_options(options_str):
     for token in tokens:
         [key, val] = token.split(':')
         options_dict[key] = val
-    if 'dest' not in options_dict:
-        options_dict['dest'] = DEFAULT_DEST
-    if 'src' not in options_dict:
-        options_dict['src'] = DEFAULT_SRC    
-    return options_dict
+    return set_defaults(options_dict)
 
 def update_options(sender_id, options):
+    options = set_defaults(options)
+    print('Inside update_options, options: ')
+    print(options)
     q = 'INSERT OR IGNORE INTO users (id, src, dest) VALUES (?, ?, ?)'
-    conn.execute(q, [str(sender_id), options['src'], options['dest']])
+    conn.execute(q, (str(sender_id), options['src'], options['dest']))
     q = 'UPDATE users SET src=?, dest=? where id = ?'
-    conn.execute(q, [options['src'], options['dest'], str(sender_id)])
+    conn.execute(q, (options['src'], options['dest'], str(sender_id)))
     print('Updated users options. Src: %s, Dest: %s' % (options['src'], options['dest']))
-
+    conn.commit()
 def get_options(sender_id):
     q = 'SELECT src, dest from users where id=?'
-    (src, dest) = conn.execute(q, [str(sender_id)])
-    return (src, dest)
+    res = conn.execute(q, (str(sender_id)))
+    print(res)
+    return default_opts
+    # return (src, dest)
+
+
+default_opts = {
+    'src': 'auto',
+    'dest': 'en'
+}
